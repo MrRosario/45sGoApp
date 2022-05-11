63649bcc4769055268c107c164c0e4fe5d4bf588
mrrosario <eugenio.dorosario94@gmail.com>
2022-05-10 23:06:31 -0300

    home screen

--- src/ui/Home.tsx
+++ src/ui/Home.tsx
@@ -1,18 +1,54 @@
-import React from "react";
+import React, { FC } from "react";
 import { View, StyleSheet, Text } from "react-native";
 
-const Home = () => {
+import { Font } from '../styles';
+
+import { Card, Button } from '../components'
+
+const Home: FC = () => {
     return(
         <View style={styles.container}>
-            <Text>Ola Home</Text>
+            <View style={styles.titleWrapper}>
+                <Text style={styles.title}>
+                    Vamos Exercitar!
+                </Text>
+            </View>
+
+            <View style={styles.content}>
+                <Card type='prepare' title='Preparar' time='00:05' />
+                <Card type='exercise' title='Exercitar' time='00:45' />
+                <Card type='rest' title='Descansar' time='00:30' />
+                <Card type='rounds' title='Rodadas' time='1' />
+            </View>
+
+            <Button 
+                size='large' 
+                title='INICIAR'  
+                callBack={() => console.log('INICIAR Exercicio')} 
+            />
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1
-    }
+        flex: 1,
+        alignItems: 'center'
+    },
+    content: {
+        flexDirection: 'column',
+        width: '100%',
+        paddingHorizontal: 33,
+    },
+    titleWrapper: {
+        marginVertical: 40,
+    },
+    title: {
+        fontFamily: Font.FAMILY.BOLD,
+        fontWeight: Font.WEIGHT.BOLD,
+        fontSize: Font.SIZE.SIZE_25,
+        color: '#000000',
+    },
 });
 
 export default Home;
