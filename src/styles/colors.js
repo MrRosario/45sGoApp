0b9eaf24445eb397197029d7d1ea8b04f481fdc8
mrrosario <eugenio.dorosario94@gmail.com>
2022-05-10 23:07:44 -0300

    app resources

---
+++ src/styles/colors.js
@@ -0,0 +1,21 @@
+const Colors = {
+  PRIMARY: "#318E46",
+  SECONDARY: "rgba(198,198,198,0.5)",
+  TERCEARY: "#6C6C6C",
+  GREY_45: "#737373",
+  WHITE: "#FFFFFF",
+  BLACK: "#000000",
+  TRANSPARENT: "rgba(0,0,0,0)",
+  LIGHT_GREY: "rgba(200,200,200, .5)",
+  DIM_GREY: "#6C6C6C",
+  SILVER_SAND: "#C4C4C4",
+  BALCK_OPAQUE: 'rgba(0,0,0,.8)',
+  PRIMARY_OPAQUE: 'rgba(49,142, 70, .8)',
+  ACTIONS: {
+    SUCCESS: "#22bb33",
+    WARNING: "#f0ad4e",
+    ALERT: "#bb2124",
+  },
+};
+
+export default Colors;
---
+++ src/styles/typography.js
@@ -0,0 +1,38 @@
+import { scaleFont } from "./mixins";
+
+const Font = {
+  FAMILY: {
+    REGULAR: "Ubuntu-Regular",
+    Light: "Ubuntu-Light",
+    BOLD: "Ubuntu-Bold",
+    MEDIUM: "Ubuntu-Medium",
+  },
+  WEIGHT: {
+    LIGHT: "300",
+    REGULAR: "400",
+    MEDIUM: "500",
+    SEMI_BOLD: "600",
+    BOLD: "700",
+    EXTRA_BOLD: "800",
+    BLACK: "900",
+  },
+  SIZE: {
+    SIZE_10: scaleFont(10),
+    SIZE_12: scaleFont(12),
+    SIZE_14: scaleFont(14),
+    SIZE_16: scaleFont(16),
+    SIZE_18: scaleFont(18),
+    SIZE_20: scaleFont(20),
+    SIZE_22: scaleFont(22),
+    SIZE_25: scaleFont(25),
+    SIZE_30: scaleFont(30),
+  },
+  LINE_HEIGHT: {
+    _16: scaleFont(16),
+    _20: scaleFont(20),
+    _23: scaleFont(23),
+    _24: scaleFont(24),
+  },
+};
+
+export default Font;
