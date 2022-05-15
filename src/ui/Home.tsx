a7e4560ee034b2abfb6b7231080fe4f5c9700b7d
mrrosario <eugenio.dorosario94@gmail.com>
2022-05-14 23:32:43 -0300

    modal time editor created

--- src/ui/Home.tsx
+++ src/ui/Home.tsx
@@ -1,57 +1,88 @@
-import React, { FC } from "react";
+import React, { useState, FC } from "react";
 import { View, StyleSheet, Text } from "react-native";
 
-import { Font } from '../styles';
+import { Font } from "../styles";
 
-import { Card, Button,TimerEditor } from '../components'
+import { Card, Button, TimerEditor } from "../components";
 
 const Home: FC = () => {
-    return(
-        <>
-            <View style={styles.container}>
-                <View style={styles.titleWrapper}>
-                    <Text style={styles.title}>
-                        Vamos treinar!
-                    </Text>
-                </View>
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+  const [modalTitle, setModalTitle] = useState("");
 
-                <View style={styles.content}>
-                    <Card type='prepare' title='Preparar' time='00:05' />
-                    <Card type='exercise' title='Exercitar' time='00:45' />
-                    <Card type='rest' title='Descansar' time='00:30' />
-                    <Card type='rounds' title='Rodadas' time='1' />
-                </View>
+  const handlePressCard = (title: any) => {
+    setIsModalVisible(true);
+    setModalTitle(title);
+  };
 
-                <Button 
-                    size='large' 
-                    title='INICIAR'  
-                    callBack={() => console.log('INICIAR Exercicio')} 
-                />
-            </View>
-            <TimerEditor modalTitle="Preparar" />
-        </>
-    )
-}
+  return (
+    <>
+      <View style={styles.container}>
+        <View style={styles.titleWrapper}>
+          <Text style={styles.title}>Vamos treinar!</Text>
+        </View>
+
+        <View style={styles.content}>
+          <Card
+            type="prepare"
+            title="Preparar"
+            time="00:05"
+            callback={handlePressCard}
+          />
+          <Card
+            type="exercise"
+            title="Exercitar"
+            time="00:45"
+            callback={handlePressCard}
+          />
+          <Card
+            type="rest"
+            title="Descansar"
+            time="00:30"
+            callback={handlePressCard}
+          />
+          <Card
+            type="rounds"
+            title="Rodadas"
+            time="1"
+            callback={handlePressCard}
+          />
+        </View>
+
+        <Button
+          size="large"
+          title="INICIAR"
+          callBack={() => console.log("INICIAR Exercicio")}
+        />
+      </View>
+      {isModalVisible && (
+        <TimerEditor
+          modalTitle={modalTitle}
+          setIsModalVisible={setIsModalVisible}
+        />
+      )}
+    </>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center'
-    },
-    content: {
-        flexDirection: 'column',
-        width: '100%',
-        paddingHorizontal: 33,
-    },
-    titleWrapper: {
-        marginVertical: 40,
-    },
-    title: {
-        fontFamily: Font.FAMILY.BOLD,
-        fontWeight: Font.WEIGHT.BOLD,
-        fontSize: Font.SIZE.SIZE_25,
-        color: '#000000',
-    },
+  container: {
+    flex: 1,
+    alignItems: "center",
+  },
+  content: {
+    flexDirection: "column",
+    width: "100%",
+    paddingHorizontal: 33,
+  },
+  titleWrapper: {
+    marginVertical: 40,
+  },
+  title: {
+    fontFamily: Font.FAMILY.BOLD,
+    fontWeight: Font.WEIGHT.BOLD,
+    fontSize: Font.SIZE.SIZE_25,
+    color: "#000000",
+  },
 });
 
 export default Home;
